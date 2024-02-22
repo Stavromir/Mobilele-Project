dda8dc7c6efcdc458b3fc22ba81acfd4ef19f3f8
Stavromir <s.kasapov@protonmail.com>
2024-02-22 12:50:46 +0200

    User entity and dto create

---
+++ src/main/java/bg/softuni/mobilele/model/dto/UserLoginDto.java
@@ -0,0 +1,5 @@
+package bg.softuni.mobilele.model.dto;
+
+public record UserLoginDto(String email,
+                           String password) {
+}
---
+++ src/main/java/bg/softuni/mobilele/model/entity/User.java
@@ -0,0 +1,63 @@
+package bg.softuni.mobilele.model.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "users")
+public class User extends BaseEntity{
+
+    private String email;
+    private String password;
+    private String firstName;
+    private String lastName;
+    private Boolean active;
+
+    public User() {
+        super();
+    }
+
+    @Column(nullable = false, unique = true)
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Column(nullable = false)
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Boolean getActive() {
+        return active;
+    }
+
+    public void setActive(Boolean active) {
+        this.active = active;
+    }
+}
